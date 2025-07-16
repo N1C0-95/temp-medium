3dffabdb7fa0f7a2b681c1a9e8b107b743fdf696
Nicolò Ferranti <nico.ferranti95@gmail.com>
2025-07-16 22:44:34 +0200

    add fluentui

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -3,17 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   distDir: 'build',
-  output: 'standalone'
-  
-  /* config options here */
+  output: 'standalone',
+  experimental: {
+    swcPlugins: [
+      ['fluentui-next-appdir-directive', { paths: ['@griffel', '@fluentui'] }],
+    ],
+  },
 };
+
 export default nextConfig;
-
-// const nextConfig = {
-//   reactStrictMode: true,
-//   distDir: 'build',
-//   output: 'standalone',
-//   // config options here
-// };
-
-// module.exports = nextConfig;
